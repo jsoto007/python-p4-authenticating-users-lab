bd9742c48d65ce6ce145ff42db238b4e5c96ebdf
jsoto007 <jsoto7087@gmail.com>
2025-05-04 18:22:22 -0400

    completed assignment

--- server/app.py
+++ server/app.py
@@ -59,11 +59,25 @@ class Login(Resource):
 
         return user.to_dict(), 200
     
+class Logout(Resource):
 
+    def delete(self):
+        session['user_id'] = None
+        return {}, 204
+    
+class CheckSession(Resource):
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.filter(User.id == user_id).first()
+            return user.to_dict(), 200
+        
+        return {}, 401
     
     
-
+api.add_resource(CheckSession, '/check_session')
 api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
